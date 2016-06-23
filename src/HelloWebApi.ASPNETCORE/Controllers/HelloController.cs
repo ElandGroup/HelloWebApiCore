15253ed068b0ab04ee60e739b1bbd9b080f71263
unknown <xiaoxm_001@163.com>
2016-06-24 00:01:41 +0800

    webapi

---
+++ src/HelloWebApi.ASPNETCORE/Controllers/HelloController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloWebApi.ASPNETCORE.Models;
+using HelloWebApi.ASPNETCORE.Repositorys;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloWebApi.ASPNETCORE.Controllers
+{
+    [Route("api/[controller]")]
+    public class HelloController : Controller
+    {
+        public IContactsService ContactsRepo { get; set; }
+
+        public HelloController(IContactsService _repo)
+        {
+            ContactsRepo = _repo;
+        }
+
+        [HttpGet]
+        public IEnumerable<Contacts> GetAll()
+        {
+            return ContactsRepo.GetAll();
+        }
+
+        [HttpGet("{id}", Name = "GetContacts")]
+        public IActionResult GetById(string id)
+        {
+            var item = ContactsRepo.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Contacts item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            ContactsRepo.Add(item);
+            return CreatedAtRoute("GetContacts", new { Controller = "Contacts", id = item.MobilePhone }, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(string id, [FromBody] Contacts item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            var contactObj = ContactsRepo.Find(id);
+            if (contactObj == null)
+            {
+                return NotFound();
+            }
+            ContactsRepo.Update(item);
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            ContactsRepo.Remove(id);
+        }
+    }
+}
---
+++ src/HelloWebApi.ASPNETCORE/Service/IContactsService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloWebApi.ASPNETCORE.Models;
+
+namespace HelloWebApi.ASPNETCORE.Repositorys
+{
+    public interface IContactsService
+    {
+        void Add(Contacts item);
+        IEnumerable<Contacts> GetAll();
+        Contacts Find(string key);
+        void Remove(string Id);
+        void Update(Contacts item);
+    }
+}
